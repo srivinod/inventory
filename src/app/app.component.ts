0d23078a237fcc08e834864b1a60998c15624af3
srivinod <vinodsemailbox@gmail.com>
2018-03-28 17:07:42 +0530

    PriceList Component Created and List Fetched

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
-
+import { Product } from './product.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent { 
+  
+  products: Product[];
+
+  constructor(){
+    let newProduct = [
+      new Product(
+      'NICEHAT',
+      'A Nice Black Shoes',
+      '/assets/images/products/blackwhite.jpg',
+      ['Men','Accessories','Shoes'],
+      29.99
+      ),
+      new Product(
+        'SANDALSHOES',
+        'A Nice Sandal Shoes',
+        '/assets/images/products/sandalshoe.jpg',
+        ['Men','Accessories','Shoes'],
+        109.99
+        ),
+        new Product(
+          'SUPERCROCS',
+          'A Nice Crocs',
+          '/assets/images/products/crocs.jpg',
+          ['Men','Accessories','Crocs'],
+          109.99
+          ),
+    ]
+    this.products = newProduct;
+  }
+
+
+  productWasSelected(product: Product): void {
+    console.log('Product Clicked', product);    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { ProductsListComponent } from './products-list/products-list.component'; 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsListComponent, 
   ],
   imports: [
     BrowserModule,
