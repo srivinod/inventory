5a0af5fcfa2cacc21e48a9982f31fad49a951957
srivinod <vinodsemailbox@gmail.com>
2018-03-29 11:52:07 +0530

    Changed app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,21 +14,21 @@ export class AppComponent {
       new Product(
       'NICEHAT',
       'A Nice Black Shoes',
-      '/assets/images/products/blackwhite.jpg',
+      'assets/images/products/blackwhite.jpg',
       ['Men','Accessories','Shoes'],
       29.99
       ),
       new Product(
         'SANDALSHOES',
         'A Nice Sandal Shoes',
-        '/assets/images/products/sandalshoe.jpg',
+        'assets/images/products/sandalshoe.jpg',
         ['Men','Accessories','Shoes'],
         109.99
         ),
         new Product(
           'SUPERCROCS',
           'A Nice Crocs',
-          '/assets/images/products/crocs.jpg',
+          'assets/images/products/crocs.jpg',
           ['Men','Accessories','Crocs'],
           109.99
           ),
